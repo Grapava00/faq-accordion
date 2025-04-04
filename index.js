08f046cdccb2eb451d4462605fbffb7626b2f50c
Giorgi Rapava <grapava00@gmail.com>
2025-04-04 20:09:08 +0400

    remove unused console log and simplify accordion click event handling

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
   const accordionHeaders = document.querySelectorAll("article section h2");
-  console.log(accordionHeaders);
   accordionHeaders.forEach((header) => {
     header.addEventListener("click", () => {
       const currentSection = header.parentElement;
-      const isActive = currentSection.classList.contains("active");
-
-      if (!isActive) {
-        currentSection.classList.add("active");
-      } else {
-        currentSection.classList.remove("active");
-      }
     });
   });
 });
